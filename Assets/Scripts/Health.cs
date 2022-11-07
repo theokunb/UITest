8aadf689a39fab8a268ecbe257e157afc241aebc
Алексей Макаров <33080157+theokunb@users.noreply.github.com>
2022-11-07 22:17:47 +0900

    блин я задание не до конца прочитал =((

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using System;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth;
+
+    public Action HelathChanged;
+    public float CurrentHealth { get; private set;}
+    public float MaxHealth => _maxHealth;
+
+    private void Start()
+    {
+        CurrentHealth = _maxHealth;
+    }
+
+    public void AddValue(float value)
+    {
+        CurrentHealth += value;
+
+        if(CurrentHealth > _maxHealth)
+        {
+            CurrentHealth = _maxHealth;
+        }
+        HelathChanged?.Invoke();
+    }
+
+    public void ReduceValue(float value)
+    {
+        CurrentHealth -= value;
+
+        if(CurrentHealth < 0)
+        {
+            CurrentHealth = 0;
+        }
+        HelathChanged?.Invoke();
+    }
+}
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,19 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections;
 
 [RequireComponent(typeof(Slider))]
 public class HealthBar : MonoBehaviour
 {
+    [SerializeField] private Health _playerHealth;
     [SerializeField] private TMP_Text _displayValue;
+    [SerializeField] private float _changeRate;
 
     private Slider _bar;
+
+    private void OnEnable()
+    {
+        _playerHealth.HelathChanged += HealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        _playerHealth.HelathChanged -= HealthChanged;
+    }
 
     private void Start()
     {
         _bar = GetComponent<Slider>();
+        _bar.maxValue = _playerHealth.MaxHealth;
+        _bar.value = _playerHealth.CurrentHealth;
     }
 
     private void Update()
@@ -21,13 +34,17 @@ public class HealthBar : MonoBehaviour
         _displayValue.text = $"{_bar.value}/{_bar.maxValue}";
     }
 
-    public void TakeDamage(float value)
+    private void HealthChanged()
     {
-        _bar.value -= value;
+        StartCoroutine(ChangeValue());
     }
 
-    public void TakeHealing(float value)
+    private IEnumerator ChangeValue()
     {
-        _bar.value += value;
+        while(_bar.value != _playerHealth.CurrentHealth)
+        {
+            _bar.value = Mathf.MoveTowards(_bar.value, _playerHealth.CurrentHealth, _changeRate * Time.deltaTime);
+            yield return null;
+        }
     }
 }
